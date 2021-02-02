3c3a91c7b1955e0571dc85bc4e99c5b4a3a06afc
leonluleonlu <leonluleonlu@gmail.com>
2021-02-02 09:40:57 +0800

    Successfully generated fractal images in console

--- main.js
+++ main.js
@@ -1,19 +1,24 @@
+// return the product of two complex numbers
 function complex_product(c1, c2) {
 	return [c1[0] * c2[0] - c1[1] * c2[1], c1[0] * c2[1] + c2[0] * c1[1]]
 }
 
+// returns the sum of two complex numbers
 function complex_addition(complex1, complex2) {
 	return [complex1[0] + complex2[0], complex1[1] + complex2[1]]
 }
 
+// returns the value after one iteration given complex number z and c
 function mandelbrot_altorhthm(z, c) {
 	return complex_addition(complex_product(z, z), c)
 }
 
+// returns the magnitude of complex number c
 function complex_magnitude_squared(c) {
 	return c[0] * c[0] + c[1] * c[1]
 }
 
+// function to return number of iterations processed before the magnitude of f(z) escapes
 function escape_time(c, iteration_depth, formula) {
 	var z = [0, 0]
 	var i = 1
@@ -29,5 +34,60 @@ function escape_time(c, iteration_depth, formula) {
 	}
 }
 
-console.log(escape_time([0.3, 0], 100, mandelbrot_altorhthm))
+// pixel manipulation
+// var main_canvas = document.getElementById('fractal_viewport');
+// var canvas_width = main_canvas.width;
+// var canvas_height = main_canvas.height;
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+// process rendering parameters
+var x_min = -2
+var x_max = 2
+var y_min = -2
+var y_max = 2
+var x_dim = 240
+var y_dim = 120
+var x_step = (x_max - x_min) / x_dim
+var y_step = (y_max - y_min) / y_dim
+var renderer_console_output = ""
+
+// logging
+console.log("Start rendering")
+console.log("x_min: " + x_min)
+console.log("x_max: " + x_max)
+console.log("y_min: " + y_min)
+console.log("y_max: " + y_max)
+console.log("x_dim: " + x_dim)
+console.log("y_dim: " + y_dim)
+console.log("x_step: " + x_step)
+console.log("y_step: " + y_step)
+
+// iterate by rows
+var imaginary = y_max
+for (let i = 1; i <= y_dim; i++) {
+	var real = x_min
+	for (let i = 1; i <= x_dim; i++) {
+			if(escape_time([real, imaginary], 100, mandelbrot_altorhthm) == Infinity) {
+				renderer_console_output = renderer_console_output + "X"
+			}
+			else {
+				renderer_console_output = renderer_console_output + " "
+			}
+		real = real + x_step
+	}
+	imaginary = imaginary - y_step
+	renderer_console_output = renderer_console_output + "\n"
+}
+console.log(renderer_console_output)
